661593f696974d0d1c34329e26465342423aab24
Kevin <kfaucher@dtcc.edu>
2024-01-10 20:19:32 -0500

    updates

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -2,13 +2,15 @@ const router = require("express").Router();
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-
 router.route("/").get(controller.list).all(methodNotAllowed);
 
-router.route("/:movieId([0-9]+)/theaters").get(controller.list).all(methodNotAllowed);
+// Corrected to use the listTheaterMovies controller for '/:movieId/theaters'
+router.route("/:movieId([0-9]+)/theaters").get(controller.listTheaterMovies).all(methodNotAllowed);
 
+// Corrected to use the read controller for '/:movieId'
 router.route("/:movieId([0-9]+)").get(controller.read).all(methodNotAllowed);
 
-router.route("/:movieId([0-9]+)/reviews").get(controller.list).all(methodNotAllowed);
+// Corrected to use the listReviews controller for '/:movieId/reviews'
+router.route("/:movieId([0-9]+)/reviews").get(controller.listReviews).all(methodNotAllowed);
 
 module.exports = router;
--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -2,9 +2,15 @@ const router = require("express").Router();
 const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").post(controller.create).all(methodNotAllowed);
+router.route("/")
+    .post(controller.create)
+    .all(methodNotAllowed);
 
-router.route("/:reviewsId").get(controller.read).put(controller.update).delete(controller.delete).all(methodNotAllowed);
-
+// Corrected 'reviewsId' to 'reviewId' to match the controller parameter expectation
+router.route("/:reviewId")
+    .get(controller.read)
+    .put(controller.update)
+    .delete(controller.delete)
+    .all(methodNotAllowed);
 
 module.exports = router;
